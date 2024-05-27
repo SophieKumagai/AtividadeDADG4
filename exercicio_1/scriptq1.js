4189eefecef6fd1270f2154bcf627792c7045ac9
SophieKumagai <sophie.satie20@gmail.com>
2024-05-27 11:37:17 -0300

    adicionando transição

--- exercicio_1/scriptq1.js
+++ exercicio_1/scriptq1.js
@@ -1,6 +1,14 @@
 // adicionando som
 let audio = new Audio('../assets/batalha.mp3');
+const circle = document.querySelector("#circle_obj")
 
 window.addEventListener('load', () => {
     audio.play();
+    circle.classList.add("circle")
 })
+
+setTimeout(function() {
+    const div_tutorial = document.createElement("div")
+    div_tutorial.id = "tutorial"
+    circle.appendChild(div_tutorial)
+}, 1800); // 5000 milissegundos = 5 segundos
