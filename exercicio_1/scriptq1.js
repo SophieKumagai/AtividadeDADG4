1b2d0e6494ee50be4a0d752e02fcfe1e56d6fb86
SophieKumagai <sophie.satie20@gmail.com>
2024-05-28 13:21:02 -0300

    tutorial

--- exercicio_1/scriptq1.js
+++ exercicio_1/scriptq1.js
@@ -10,5 +10,22 @@ window.addEventListener('load', () => {
 setTimeout(function() {
     const div_tutorial = document.createElement("div")
     div_tutorial.id = "tutorial"
+    div_tutorial.style.position = "absolute"
+    div_tutorial.style.top = "auto"
+    div_tutorial.style.left = "auto"
+    div_tutorial.style.width = "100%"
+    div_tutorial.style.height = "100%"
+    div_tutorial.style.zIndex = "999"
+
+    const iframe = document.createElement("iframe")
+    iframe.style.position = "absolute"
+    iframe.style.top = "auto"
+    iframe.style.left = "auto"
+    iframe.style.width = "100%"
+    iframe.style.height = "100%"
+    iframe.style.zIndex = "999"
+    iframe.setAttribute("src", "../tutorial/index_tutorial.html")
+
+    div_tutorial.appendChild(iframe)
     circle.appendChild(div_tutorial)
-}, 1800); // 5000 milissegundos = 5 segundos
+}, 1800); // 1800 milissegundos
